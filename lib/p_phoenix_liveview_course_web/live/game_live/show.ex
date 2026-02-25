de0ad19691b242d4f553328581bbe96dfa4dfbca
Ernesto <ernesto.cuapantecatl@bunsan.io>
2026-02-24 22:30:03 -0600

    Implement real-time view tracking

--- lib/p_phoenix_liveview_course_web/live/game_live/show.ex
+++ lib/p_phoenix_liveview_course_web/live/game_live/show.ex
@@ -3,18 +3,32 @@ defmodule PPhoenixLiveviewCourseWeb.GameLive.Show do
 
   alias PPhoenixLiveviewCourse.Catalog
   alias PPhoenixLiveviewCourseWeb.GameLive.Tomatometer
+  alias PPhoenixLiveviewCourseWeb.GameLive.ViewCounterComponent
 
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(PPhoenixLiveviewCourse.PubSub, "game_views")
+    end
     {:ok, socket}
   end
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    {:ok, game} = Catalog.increment_game_views(id)
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:game, Catalog.get_game!(id))}
+  end
+
+  @impl true
+  def handle_info({:game_updated, updated_game}, socket) do
+    if updated_game.id == socket.assigns.game.id do
+      {:noreply, assign(socket, :game, updated_game)}
+    else
+      {:noreply, socket}
+    end
   end
 
   @impl true
